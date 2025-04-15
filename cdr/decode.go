045b1e4ee196758a1b01ea55294047e6f60d7c14
Ian Chen <ychen.cs10@nycu.edu.tw>
2025-04-15 16:48:14 +0800

    fix: parsing error

--- cdr/decode.go
+++ cdr/decode.go
@@ -67,7 +67,7 @@ func ParseCdr(content []byte) *ThreegppCdr {
 	file := NewThreegppCdr()
 	err := file.Read(kaitai.NewStream(bytes.NewReader(content)), nil, file)
 	if err != nil {
-		fmt.Println("Failed to parse CDR file.")
+		fmt.Printf("Failed to parse CDR file: %v", err)
 		os.Exit(2)
 	}
 	return file
